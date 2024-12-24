8d932bf9621961b54ca7772cb290bab5db51fbe3
Dimitris <crazyspirit1993@gmail.com>
2024-12-24 12:44:30 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/home", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  res.render("index.ejs", {name:req.body["fname"]});
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
